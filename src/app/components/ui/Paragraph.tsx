c61b2f71ad9b67238fe3f9ce889ff3d9eb6de0b2
Minh Tu Ngo <loducfc@gmail.com>
2023-03-10 11:09:33 -0500

    feat: add paragraph components

--- src/app/components/ui/Paragraph.tsx
+++ src/app/components/ui/Paragraph.tsx
@@ -1,9 +1,35 @@
-import { FC } from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
+import { FC, forwardRef, HTMLAttributes } from 'react';
 
-interface ParagraphProps {}
+const paragraphVariants = cva(
+  'max-w-prose text-slate-700 darkL:text-slate-300 mb-2 text-center',
+  {
+    variants: {
+      size: {
+        default: 'text-base sm:text-lg',
+        sm: 'text-sm sm:text-base',
+      },
+    },
+    defaultVariants: {
+      size: 'default',
+    },
+  }
+);
 
-const Paragraph: FC<ParagraphProps> = ({}) => {
-  return <div>Paragraph</div>;
-};
+interface ParagraphProps
+  extends HTMLAttributes<HTMLParagraphElement>,
+    VariantProps<typeof paragraphVariants> {}
+
+const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps>(
+  ({ className, size, children, ...props }, ref) => {
+    return (
+      <p ref={ref} {...props} className={}>
+        Paragraph
+      </p>
+    );
+  }
+);
+
+Paragraph.displayName = 'Paragraph';
 
 export default Paragraph;
