fe9c1b3d9871e705be3e2ef07e4a9f5163c8b4a3
Minh Tu Ngo <loducfc@gmail.com>
2023-03-10 23:47:34 -0500

    feat: add button component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import { getServerSession } from 'next-auth';
+import Link from 'next/link';
+import { FC } from 'react';
+import { buttonVariants } from '@/components/ui/Button';
+
+const Navbar = async ({}) => {
+  const session = await getServerSession();
+  return (
+    <div className='back-drop-blur-sm dark:border-state-700 fixed top-0 left-0 right-0 z-50 flex h-20 items-center justify-between border-b border-slate-300 bg-white/75 shadow-sm dark:bg-slate-900'>
+      <div className='container mx-auto flex w-full max-w-7xl items-center justify-between'>
+        <Link href='/' className={buttonVariants({ variant: 'link' })}>
+          Text Similarity
+        </Link>
+        <div className='md:hidden'>
+          <ThemeToggle />
+        </div>
+        <div className='hidden gap-4 md:flex'>
+          <ThemeToggle />
+          <Link href='/' className={buttonVariants({ variant: 'ghost' })}>
+            Documentation
+          </Link>
+          {session ? (
+            <>
+              <Link
+                href='/dashboard'
+                className={buttonVariants({ variant: 'ghost' })}
+              >
+                Dashboard
+              </Link>
+              <SignOutButton />
+            </>
+          ) : (
+            <SignInButton />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,60 @@
+import { mergeClassNames } from '@/lib/utils';
+import { cva, VariantProps } from 'class-variance-authority';
+import { ButtonHTMLAttributes, FC } from 'react';
+import { Loader2 } from 'lucide-react';
+
+export const buttonVariants = cva(
+  'active:scale-95 inline-flex items-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-slate-50 focus:ring-slate-400 disabled:opacity-50 disabled:cursor-not-allowed disabled:pointer-events-none dark:focus:ring-offset-slate-900 dark:focus:ring-slate-500',
+  {
+    variants: {
+      variant: {
+        default:
+          'bg-slate-900 text-gray-50 hover:bg-slate-800 dark:bg-slate-200 dark:text-slate-900 dark:hover:bg-slate-100',
+        outline:
+          'bg-slate-900 text-gray-50 hover:bg-slate-900 dark:bg-slate-200 dark:text-slate-900 dark:hover:bg-slate-100 border border-slate-200 hover:bg-slate-100 dark:border-slate-700',
+        ghost:
+          'bg-transparent hover:bg-slate-100 dark:hover:bg-slate-800 dark:text-slate-400 data-[state=open]:bg-transparent dark:data-[state=open]:bg-transparent',
+        link: 'bg-transparent dark:bg-transparent underline-offset-4 hover:underline text-slate-900 dark:text-slate-100 hover:bg-transparent dark:hover:bg-transparent',
+      },
+      size: {
+        default: 'h-10 px-4 py-2',
+        sm: 'h-9 px-2 rounded-md',
+        lg: 'h-11 px-8 rounded-md',
+      },
+      defaultVariant: {
+        variant: 'default',
+        size: 'default',
+      },
+    },
+  }
+);
+
+interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
+}
+
+const Button: FC<ButtonProps> = ({
+  className,
+  children,
+  variant,
+  isLoading,
+  size,
+  ...props
+}) => {
+  return (
+    <button
+      className={mergeClassNames(buttonVariants({ variant, size, className }))}
+      disabled={isLoading}
+      {...props}
+    >
+      {isLoading ? <Loader2 className='mr-2 h-4 w-4 animate-spin' /> : null}
+      {children}
+    </button>
+  );
+};
+
+Button.displayName = 'Button';
+
+export default Button;
