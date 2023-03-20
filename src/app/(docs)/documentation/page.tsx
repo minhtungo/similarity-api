271b7f687df1676b9d86e8401a4f45374799d870
Minh Tu Ngo <loducfc@gmail.com>
2023-03-20 18:40:43 -0400

    style: styling documentation page, add code animation

--- src/app/(docs)/documentation/page.tsx
+++ src/app/(docs)/documentation/page.tsx
@@ -3,21 +3,23 @@ import { FC } from 'react';
 import type { Metadata } from 'next';
 import Heading from '@/components/ui/Heading';
 import Paragraph from '@/components/ui/Paragraph';
+import DocumentationTabs from '@/components/DocumentationTabs';
 
 export const medata: Metadata = {
-    title: 'Similarity API | Documentation',
-    description: 'Free text similarity API',
-}
+  title: 'Similarity API | Documentation',
+  description: 'Free text similarity API',
+};
 
-const page: FC= ({}) => {
-  return <div className='container max-w-7xl mx-auto mt-12'>
-    <div className="flex flex-col items-center gap-6">
+const page: FC = ({}) => {
+  return (
+    <div className='container mx-auto mt-12 max-w-7xl'>
+      <div className='flex flex-col items-center gap-6'>
         <Heading>Making a request</Heading>
         <Paragraph>api/v1/similarity</Paragraph>
-
-        
+        <DocumentationTabs />
+      </div>
     </div>
-  </div>
-}
+  );
+};
 
-export default page
+export default page;
