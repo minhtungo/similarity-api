02d4ed9c8f71ab3d92ef7ab92e5f0a4ce6a0623c
Minh Tu Ngo <loducfc@gmail.com>
2023-03-20 19:29:53 -0400

    feat: implement authentication, configure prisma

---
+++ src/lib/auth.ts
@@ -0,0 +1,36 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { NextAuthOptions } from 'next-auth';
+import { db } from '@/lib/db';
+import GoogleProvider from 'next-auth/providers/google';
+
+const getGoogleCredentials = () => {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+  if (!clientId || clientId.length === 0) {
+    throw new Error('Missing GOOGLE_CLIENT_ID');
+  }
+
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error('Missing GOOGLE_CLIENT_SECRET');
+  }
+
+  return { clientId, clientSecret };
+};
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/login',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: getGoogleCredentials().clientId,
+      clientSecret: getGoogleCredentials().clientSecret,
+    }),
+    // ...add more providers here
+  ],
+  
+};
