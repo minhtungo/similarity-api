7f434114135dbd2c022a728621a2af7d41adf187
Minh Tu Ngo <loducfc@gmail.com>
2023-03-21 01:47:48 -0400

    refactor: move api to pages folder as nextauth not supporting new api handler

--- src/app/api/api-key/create/routes.ts
+++ src/app/api/api-key/create/routes.ts
@@ -15,6 +15,7 @@ export async function GET(
     const user = await getServerSession(request, response, authOptions).then(
       (res) => res?.user
     );
+
     if (!user) {
       return NextResponse.json(
         { error: 'Not authorized', createdApiKey: null },
