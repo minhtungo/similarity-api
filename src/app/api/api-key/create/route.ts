8969a22c9264eb3ba24c62f080e1e09836b5262a
Minh Tu Ngo <loducfc@gmail.com>
2023-03-21 01:29:38 -0400

    feat: implement create API route

---
+++ src/app/api/api-key/create/route.ts
@@ -0,0 +1,55 @@
+import { authOptions } from '@/lib/auth';
+import { db } from '@/lib/db';
+import { CreateApiData } from '@/types/api';
+import { nanoid } from 'nanoid';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import { NextResponse, NextRequest } from 'next/server';
+import { z } from 'zod';
+
+export async function GET(
+  request: NextApiRequest,
+  response: NextApiResponse<CreateApiData>
+) {
+  try {
+    const user = await getServerSession(request, response, authOptions).then(
+      (res) => res?.user
+    );
+    if (!user) {
+      return NextResponse.json(
+        { error: 'Not authorized', createdApiKey: null },
+        { status: 401 }
+      );
+    }
+    const existingApiKey = await db.apiKey.findFirst({
+      where: { userId: user.id, enabled: true },
+    });
+    if (existingApiKey) {
+      return NextResponse.json(
+        {
+          error: 'You already have an API key.',
+          createdApiKey: null,
+        },
+        { status: 400 }
+      );
+    }
+    const createdApiKey = await db.apiKey.create({
+      data: {
+        userId: user.id,
+        key: nanoid(),
+      },
+    });
+    return NextResponse.json({ error: null, createdApiKey }, { status: 401 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        { error: error.issues, createdApiKey: null },
+        { status: 400 }
+      );
+    }
+    return NextResponse.json(
+      { error: 'Internal Server Error', createdApiKey: null },
+      { status: 400 }
+    );
+  }
+}
