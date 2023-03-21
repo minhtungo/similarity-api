e8da7c6a8047e78003d88cf9257d2040e7492905
Minh Tu Ngo <loducfc@gmail.com>
2023-03-21 01:56:48 -0400

    fix: only allow Get method to create api

--- src/pages/api/api-key/create.ts
+++ src/pages/api/api-key/create.ts
@@ -6,42 +6,47 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth';
 import { z } from 'zod';
 
-export async function GET(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<CreateApiData>
 ) {
-  try {
-    const user = await getServerSession(req, res, authOptions).then(
-      (res) => res?.user
-    );
+  if (req.method === 'GET') {
+    try {
+      const user = await getServerSession(req, res, authOptions).then(
+        (res) => res?.user
+      );
 
-    if (!user) {
-      return res
-        .status(401)
-        .json({ error: 'Not authorized', createdApiKey: null });
-    }
-    const existingApiKey = await db.apiKey.findFirst({
-      where: { userId: user.id, enabled: true },
-    });
-    if (existingApiKey) {
-      return res.status(401).json({
-        error: 'You already have an API key.',
-        createdApiKey: null,
+      if (!user) {
+        return res
+          .status(401)
+          .json({ error: 'Not authorized', createdApiKey: null });
+      }
+      const existingApiKey = await db.apiKey.findFirst({
+        where: { userId: user.id, enabled: true },
       });
+      if (existingApiKey) {
+        return res.status(401).json({
+          error: 'You already have an API key.',
+          createdApiKey: null,
+        });
+      }
+      const createdApiKey = await db.apiKey.create({
+        data: {
+          userId: user.id,
+          key: nanoid(),
+        },
+      });
+      return res.status(201).json({ error: null, createdApiKey });
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return res
+          .status(400)
+          .json({ error: error.issues, createdApiKey: null });
+      }
+      return res
+        .status(400)
+        .json({ error: 'Internal Server Error', createdApiKey: null });
     }
-    const createdApiKey = await db.apiKey.create({
-      data: {
-        userId: user.id,
-        key: nanoid(),
-      },
-    });
-    return res.status(201).json({ error: null, createdApiKey });
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      return res.status(400).json({ error: error.issues, createdApiKey: null });
-    }
-    return res
-      .status(400)
-      .json({ error: 'Internal Server Error', createdApiKey: null });
   }
+  return res.status(405).end();
 }
