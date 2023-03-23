a343498468a41cbe3674b7e205f746ca4ff9f7e5
Minh Tu Ngo <loducfc@gmail.com>
2023-03-22 22:23:16 -0400

    feat: protect routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { Ratelimit } from '@upstash/ratelimit';
 import { Redis } from '@upstash/redis';
+import { getToken } from 'next-auth/jwt';
 import { withAuth } from 'next-auth/middleware';
 import { NextResponse } from 'next/server';
 
@@ -13,26 +14,62 @@ const ratelimit = new Ratelimit({
   limiter: Ratelimit.slidingWindow(15, '1 h'),
 });
 
-export default withAuth(async function middleware(req) {
-  const pathname = req.nextUrl.pathname;
-  //rate-limiting
-  if (pathname === '/api') {
-    const ip = req.ip ?? '127.0.0.1';
-    try {
-      const { success } = await ratelimit.limit(ip);
+export default withAuth(
+  async function middleware(req) {
+    const pathname = req.nextUrl.pathname;
+    //rate-limiting
+    if (pathname === '/api') {
+      const ip = req.ip ?? '127.0.0.1';
+      try {
+        const { success } = await ratelimit.limit(ip);
 
-      if (!success)
+        if (!success)
+          return NextResponse.json(
+            { error: 'Too many requests' },
+            { status: 429 }
+          );
+
+        return NextResponse.next();
+      } catch (error) {
         return NextResponse.json(
-          { error: 'Too many requests' },
-          { status: 429 }
+          { error: 'Internal Server Error' },
+          { status: 400 }
         );
-
-      return NextResponse.next();
-    } catch (error) {
-      return NextResponse.json(
-        { error: 'Internal Server Error' },
-        { status: 400 }
-      );
+      }
     }
+
+    //Make sure the user is authenticated
+    const token = await getToken({ req });
+    const isAuth = !!token;
+
+    const isAuthPage = pathname.startsWith('/login');
+
+    const protectedRoutes = ['/dashboard'];
+
+    if (isAuthPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL('/dashboard', req.url));
+      }
+
+      return null;
+    }
+
+    if (
+      !isAuth &&
+      protectedRoutes.some((route) => pathname.startsWith(route))
+    ) {
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        return true;
+      },
+    },
   }
-});
+);
+
+export const config = {
+  matcher: ['/', '/login', '/dashboard/:path*', '/api/:path*'],
+};
